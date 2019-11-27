7296b43047398d4989120b4520929d9d4c227b89
Matt Dyson <mdyson@tubi.tv>
2019-11-27 11:50:52 -0800

    add carthage support

---
+++ Carthage/InfiniteCarouselCollectionView_.h
@@ -0,0 +1,18 @@
+//
+//  InfiniteCarouselCollectionView_.h
+//  InfiniteCarouselCollectionView.
+//
+//  Created by mdyson on 11/27/19.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for InfiniteCarouselCollectionView_.
+FOUNDATION_EXPORT double InfiniteCarouselCollectionView_VersionNumber;
+
+//! Project version string for InfiniteCarouselCollectionView_.
+FOUNDATION_EXPORT const unsigned char InfiniteCarouselCollectionView_VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <InfiniteCarouselCollectionView_/PublicHeader.h>
+
+
