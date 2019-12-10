f8fb2ff79b5931045405c8b1bcd6fa402f13ae13
Matt Dyson <mdyson@tubi.tv>
2019-12-10 13:55:05 -0800

    set carthage ios min to match pod at ios 10

--- Carthage/InfiniteCarouselCollectionView_.h
+++ Carthage/InfiniteCarouselCollectionView_.h
@@ -1,10 +1,3 @@
-//
-//  InfiniteCarouselCollectionView_.h
-//  InfiniteCarouselCollectionView.
-//
-//  Created by mdyson on 11/27/19.
-//
-
 #import <Foundation/Foundation.h>
 
 //! Project version number for InfiniteCarouselCollectionView_.
@@ -12,7 +5,3 @@ FOUNDATION_EXPORT double InfiniteCarouselCollectionView_VersionNumber;
 
 //! Project version string for InfiniteCarouselCollectionView_.
 FOUNDATION_EXPORT const unsigned char InfiniteCarouselCollectionView_VersionString[];
-
-// In this header, you should import all the public headers of your framework using statements like #import <InfiniteCarouselCollectionView_/PublicHeader.h>
-
-
